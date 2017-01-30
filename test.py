5bc6c72fda5acf84954f276935ed0de59b655dbe
Ridnarong Promya <ridnarongp@yahoo.com>
2017-01-30 17:08:07 +0700

    Add requirements.txt, print error message

--- test.py
+++ test.py
@@ -7,18 +7,25 @@ import json
 from subprocess import call
 
 amqp_host = os.environ.get('AMQP_VPN_HOST', 'localhost')
-openvpn_exit = call(["openvpn", "--daemon", "--config", "connect.ovpn"])
-
+ovpn_path = os.environ.get('OVPN_PATH', 'connect.ovpn')
+vpn_exe = os.environ.get('VPN_EXE', 'openvpn')
+openvpn_exit = call(["openvpn", "--daemon", "--config", ovpn_path])
+endpoint = os.environ.get('MAKER_ENDPOINT', None)
 if openvpn_exit == 0:
     time.sleep(60)
 else:
-    call(['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', '{"value1": "OpenVPN error"}', os.environ.get('MAKER_ENDPOINT', 'http://localhost/')])
+    if endpoint:
+        call(['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', '{"value1": "OpenVPN error"}', endpoint])
+    print("OpenVPN error")
 
 ping_exit = call(["ping", "-c", "3", amqp_host])
 
 if ping_exit != 0:
     amqp_host = os.environ.get('AMQP_PUBLIC_HOST', 'localhost')
-    call(['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', '{"value1":"VPN network is not ready"}', os.environ.get('MAKER_ENDPOINT', 'http://localhost/')])
+    if endpoint:
+        call(['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', '{"value1":"VPN network is not ready"}', endpoint])
+    print("VPN network is not ready")
+
 try:
     credit = pika.credentials.PlainCredentials(os.environ.get('AMQP_USERNAME', 'guest'),
         os.environ.get('AMQP_PASSWORD', 'guest'))
@@ -32,6 +39,8 @@ try:
     channel.basic_publish(exchange='',routing_key='test',body='test')
     print("Success send message to %s" % (amqp_host))
     connection.close()
-except:
-    call(['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', '{"value1":"Unable to send message"}', os.environ.get('MAKER_ENDPOINT', 'http://localhost/')])
+except Exception as e:
+    if endpoint:
+        call(['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', "{\"value1\":\"%s\"}" % (repr(e)), endpoint])
+    print(e)
 
